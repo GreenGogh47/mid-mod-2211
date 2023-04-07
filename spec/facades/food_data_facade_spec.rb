3eeb73f4dbaf63e4d4128466e75b9e9aac8d1c1b
Chris Crane <chrisjcrane@gmail.com>
2023-04-07 10:13:36 -0600

    add test for FoodDataFacade

---
+++ spec/facades/food_data_facade_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe FoodDataFacade do
+  subject(:facade) { described_class.new}
+
+  let(:foods) { facade.get_foods_by_ingredient("potatoes") }
+
+  describe "instance methods" do
+    context "#get_foods_by_ingredient" do
+      it "returns a list of foods that include the ingredient param" do
+        expect(foods).to be_a(Array)
+        expect(foods.first).to be_a(Food)
+        expect(foods.first.gtin_upc).to eq("859546004603")
+      end
+    end
+  end
+end
