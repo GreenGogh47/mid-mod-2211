7c97601f7eab9174332e1303ef147cad9d34bfcf
Chris Crane <chrisjcrane@gmail.com>
2023-04-07 10:56:49 -0600

    create show page

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe "food index page" do
 
   describe "as a visitor" do
     it "shows a list of foods that contain the ingredient" do
-      expect(page).to_have_content("GTIN/UPC code: 859546004603")
-      expect(page).to_have_content("Description: SARATOGA CHIPS, AMERICA'S FIRST KETTLE CHIP, ALL NATURAL RED, WHITE, AND BLUE POTATO CHIPS, OLD GLORIES, OLD GLORIES")
-      expect(page).to_have_content("Brand Owner: Saratoga Chips LLC")
-      expect(page).to_have_content("Ingredients: POTATOES (WHITE POTATOES, BLUE ADIRONDACK POTATOES, RED ADIRONDACK POTATOES), SUNFLOWER OIL, HIMALAYAN SALT.")
+      expect(page).to have_content("GTIN/UPC code: 859546004603")
+      expect(page).to have_content("Description: SARATOGA CHIPS, AMERICA'S FIRST KETTLE CHIP, ALL NATURAL RED, WHITE, AND BLUE POTATO CHIPS, OLD GLORIES, OLD GLORIES")
+      expect(page).to have_content("Brand Owner: Saratoga Chips LLC")
+      expect(page).to have_content("Ingredients: POTATOES (WHITE POTATOES, BLUE ADIRONDACK POTATOES, RED ADIRONDACK POTATOES), SUNFLOWER OIL, HIMALAYAN SALT.")
     end
   end
 end
